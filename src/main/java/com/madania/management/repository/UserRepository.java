2dd021dfc9edabe93b9b4995c3c20c628f2902aa
alvinskylers <alvinskylers.dev@gmail.com>
2026-06-10 11:08:10 +0800

    add: custom user details service and fix missconfig in user repo

--- src/main/java/com/madania/management/repository/UserRepository.java
+++ src/main/java/com/madania/management/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.madania.management.repository;
 import com.madania.management.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
 }

4c06f0be4db852ea950f3efd4fd7a2e310cf88a9
alvinskylers <alvinskylers.dev@gmail.com>
2026-06-19 06:59:19 +0800

    feat: therapist template pages

--- src/main/java/com/madania/management/controller/therapist/TherapistDashboardController.java
+++ src/main/java/com/madania/management/controller/therapist/TherapistDashboardController.java
@@ -1,7 +1,6 @@
 package com.madania.management.controller.therapist;
 
 import com.madania.management.config.security.CustomUserDetails;
-import org.springframework.beans.factory.config.CustomEditorConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,10 +10,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/therapist")
 public class TherapistDashboardController {
+
     @GetMapping("/dashboard")
     public String dashboard(Authentication authentication, Model model) {
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
         model.addAttribute("role", userDetails.getUser().getRole().name());
         return "therapist/dashboard";
+    }
+
+    @GetMapping("/schedule")
+    public String schedule(Authentication authentication, Model model) {
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        model.addAttribute("role", userDetails.getUser().getRole().name());
+        return "pages/schedule";
+    }
+
+    @GetMapping("/journals")
+    public String journals(Authentication authentication, Model model) {
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        model.addAttribute("role", userDetails.getUser().getRole().name());
+        return "pages/journals";
+    }
+
+    @GetMapping("/journal")
+    public String journal(Authentication authentication, Model model) {
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        model.addAttribute("role", userDetails.getUser().getRole().name());
+        return "pages/journal";
+    }
+
+    @GetMapping("/journal/create")
+    public String createJournal(Authentication authentication, Model model) {
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        model.addAttribute("role", userDetails.getUser().getRole().name());
+        return "pages/journal-create";
+    }
+
+    @GetMapping("/patients")
+    public String patient(Authentication authentication, Model model) {
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        model.addAttribute("role", userDetails.getUser().getRole().name());
+        return "pages/patients";
     }
 }
